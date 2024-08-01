bcf4d29790b0925f144c697b5b51081d2cd20890
leesiyeon99 <qsqs9090@naver.com>
2024-08-01 23:40:53 +0900

    [Re]그래프_기본실습

---
+++ 그래프_기본학습/복습.cs
@@ -0,0 +1,67 @@
+﻿namespace 그래프_기본학습
+{
+    internal class 복습
+    {
+
+        public abstract class Graph
+        {
+            public abstract void Connect(int from, int to);
+            public abstract void DisConnect(int from, int to);
+            public abstract bool InConnect(int from, int to);
+        }
+
+        public class MatrixGraph : Graph
+        {
+            bool[,] matrixGraph;
+
+            public MatrixGraph(int vertex)
+            {
+                matrixGraph = new bool[vertex, vertex];
+            }
+
+            public override void Connect(int from, int to)
+            {
+                matrixGraph[from, to] = true;
+            }
+
+            public override void DisConnect(int from, int to)
+            {
+                matrixGraph[from, to] = false;
+            }
+
+            public override bool InConnect(int from, int to)
+            {
+                return matrixGraph[from, to];
+            }
+        }
+
+        public class ListGraph : Graph
+        {
+            List<int>[] list;
+
+            public ListGraph(int vertex)
+            {
+                list = new List<int>[vertex];
+                for (int i = 0; i < vertex; i++)
+                {
+                    list[i] = new List<int>();
+                }
+            }
+
+            public override void Connect(int from, int to)
+            {
+                list[from].Add(to);
+            }
+
+            public override void DisConnect(int from, int to)
+            {
+                list[from].Remove(to);
+            }
+
+            public override bool InConnect(int from, int to)
+            {
+                return list[from].Contains(to);
+            }
+        }
+    }
+}
