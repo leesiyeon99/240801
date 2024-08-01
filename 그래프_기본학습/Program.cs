d1b0dfc0f1d88e357aca1949a190fa78cb2af311
leesiyeon99 <qsqs9090@naver.com>
2024-08-01 17:41:21 +0900

    [Add] 그래프_기본학습, 심화학습

---
+++ 그래프_기본학습/Program.cs
@@ -0,0 +1,142 @@
+﻿namespace 그래프_기본학습
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListGraph listGraph = new ListGraph(8);
+            listGraph.Connect(0, 3);
+
+            listGraph.Connect(1, 2);
+            listGraph.Connect(1, 3);
+
+            listGraph.Connect(2, 1);
+            listGraph.Connect(2, 4);
+            listGraph.Connect(2, 6);
+
+            listGraph.Connect(3, 0);
+            listGraph.Connect(3, 1);
+            listGraph.Connect(3, 5);
+            listGraph.Connect(3, 7);
+
+            listGraph.Connect(4, 2);
+
+            listGraph.Connect(5, 3);
+
+            listGraph.Connect(6, 2);
+
+            listGraph.Connect(7, 3);
+
+            Console.WriteLine("-----리스트를 이용해 출력-----");
+            for (int from = 0; from < 8; from++)
+            {
+                Console.WriteLine($"{from} 정점: ");
+                for (int to = 0; to < 8; to++)
+                {
+                    if(listGraph.InConnect(from, to))
+                    {
+                        Console.WriteLine($"  {to} 정점");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Graph matrixGraph = new MatrixGraph(8);
+            matrixGraph.Connect(0, 3);
+
+            matrixGraph.Connect(1, 2);
+            matrixGraph.Connect(1, 3);
+
+            matrixGraph.Connect(2, 1);
+            matrixGraph.Connect(2, 4);
+            matrixGraph.Connect(2, 6);
+          
+            matrixGraph.Connect(3, 0);
+            matrixGraph.Connect(3, 1);
+            matrixGraph.Connect(3, 5);
+            matrixGraph.Connect(3, 7);
+            
+            matrixGraph.Connect(4, 2);
+            
+            matrixGraph.Connect(5, 3);
+            
+            matrixGraph.Connect(6, 2);
+            
+            matrixGraph.Connect(7, 3);
+
+            Console.WriteLine("-----인접행렬을 이용해 출력-----");
+            for (int from = 0; from < 8; from++)
+            {
+                Console.WriteLine($"{from} 정점: ");
+                for (int to = 0; to < 8; to++)
+                {
+                    if (matrixGraph.InConnect(from, to))
+                    {
+                        Console.WriteLine($"  {to} 정점");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public abstract class Graph
+        {
+            public abstract void Connect(int from, int to);
+            public abstract void DisConnect(int from, int to);
+            public abstract bool InConnect(int from, int to);
+        }
+
+        public class MatrixGraph : Graph
+        {
+            private bool[,] graph;
+            public MatrixGraph(int vertex)
+            {
+                graph = new bool[vertex, vertex];
+            }
+
+            public override void Connect(int from, int to)
+            {
+                graph[from, to] = true;
+            }
+
+            public override void DisConnect(int from, int to)
+            {
+                graph[from, to] = false;
+            }
+
+            public override bool InConnect(int from, int to)
+            {
+                return graph[from, to];
+            }
+        }
+
+        public class ListGraph : Graph
+        {
+            private List<int>[] graph;
+            public ListGraph(int vertex)
+            {
+                graph = new List<int>[vertex];
+                for (int i = 0; i < vertex; i++)
+                {
+                    graph[i] = new List<int>();
+                }
+            }
+
+            public override void Connect(int from, int to)
+            {
+                graph[from].Add(to);
+            }
+
+            public override void DisConnect(int from, int to)
+            {
+                graph[from].Remove(to);
+            }
+
+            public override bool InConnect(int from, int to)
+            {
+                return (graph[from].Contains(to));
+            }
+        }
+    }
+}
---
+++ 딕셔너리_기본실습/Program.cs
@@ -0,0 +1,50 @@
+﻿using static 딕셔너리_기본실습.Program;
+using System.Xml.Linq;
+
+namespace 딕셔너리_기본실습
+/*과제 1. 몬스터 데이터베이스 구현하기
+다음의 조건을 충족하는 프로그램을 구현하시오
+구현 클래스
+MonsterData
+Monster
+프로그램 시작 시 MonsterData는 몬스터 이름 기준의 string Key 값으로 딕셔너리에 저장한다.
+최소 5종류 이상 저장한다.
+Monster 클래스의 인스턴스 생성 시, 생성자를 사용해 딕셔너리에 저장된 MonsterData 
+클래스의 정보를 불러와 인스턴스의 데이터를 초기화해야 한다.*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MonsterData monsterData = new MonsterData();
+        }
+
+        class Monster
+        {
+            public string name;
+            public int hp;
+            public int attack;
+
+            public Monster(string name, int hp, int attack)
+            {
+                this.name = name;
+                this.hp = hp;
+                this.attack = attack;
+            }
+        }
+
+        public class MonsterData
+        {
+            Dictionary<string, Monster> monsterdata = new Dictionary<string, Monster>();
+
+            public MonsterData()
+            {
+                monsterdata.Add("오크", new Monster("오크", 100, 20));
+                monsterdata.Add("슬라임", new Monster("슬라임", 80, 10));
+                monsterdata.Add("마녀", new Monster("마녀", 120, 40));
+                monsterdata.Add("드래곤", new Monster("드래곤", 300, 100));
+                monsterdata.Add("좀비", new Monster("좀비", 70, 10));
+            }
+        }
+    }
+}
